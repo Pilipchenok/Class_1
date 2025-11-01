4413f6cd5fc64f38e5804042848a3aaf84d4b517
lavrentev.makar <makar.lavrentev@yandex.ru>
2025-11-01 12:34:41 +0300

    Add base script

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
+int** make(int r, int c);
+void output(const int*const* mtx);
+void rm(int** mtx);
+
 int main(){
-    
+    int rows = 0, cols = 0;
+    std::cin >> rows >> cols;
+    if(std::cin.fail()){
+        return 1;
+    }
+    int** mtx = nullptr;
+    mtx = make(rows, cols);
+    output(mtx);
+    rm(mtx);
 }
